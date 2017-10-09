8137ea98acd603af06bf167c4eb4cdf77c7b7310
fengxianqi <609587734@qq.com>
2017-10-10 00:01:34 +0800

    add HttpHelper.cs

---
+++ ShoppingSpider/ShoppingSpider/Common/HttpHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace ShoppingSpider.Common
+{
+    public static class HttpHelper
+    {
+        public static string GetHttpContent(string url)
+        {
+            //HttpRequest hr = new HttpRequest();
+            string str=string.Empty;
+            using (var client = new HttpClient())
+            {
+                 var responseString = client.GetStringAsync(url);
+                str = responseString.Result;
+            }
+            return str;
+        }
+    }
+}
